dfc3a3d5d2723be7b0c5cdde8ea43c1c9f8c2f40
Jan Samek <ssamcza@gmail.com>
2024-03-28 18:44:24 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Arizonia, Alex_Brush, Bad_Script, Playball, Great_Vibes, Sacramento } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const greatVibes = Great_Vibes({ weight: '400', subsets: ["latin"] });
+const sacramento = Sacramento({ weight: '400', subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={greatVibes.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,211 @@
 import Image from "next/image";
+import Link from "next/link";
+// import { StyleSheet, StyleSheetServer, css } from 'aphrodite';
+// import ReactDOMServer from 'react-dom/server';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col">
+      {/* navbar navbar */}
+      <nav className="navbar">
+        <a href="#about_us" className="navbar-text">
+          O nás
+        </a>
+        <a
+          href="#plan"
+          className="navbar-text hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          Harmonogram
+        </a>
+        <a
+          href="#map"
+          className="navbar-text hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          Kde
+        </a>
+        <a href="#colors" className="navbar-text">
+          Barvy
+        </a>
+        <a href="#form" className="navbar-text">
+          Formulář
+        </a>
+      </nav>
+      <div className="flex  flex-col items-center justify-between">
+        <h2 className="name">Ivča & Honza</h2>
+        <div className="relative place-items-center main-image">
+          <Image
+            src="/Us.jpeg"
+            alt="Thats us"
+            fill
+            // sizes="100vw"
+            style={{
+              objectFit: "cover",
+              objectPosition: "top",
+            }}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div id="about_us" className="mb-32 text-left container-section-2 ">
+        
+        <h2>Bylo nebylo</h2>
+         
+        
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          className="relative "
+          src="/divider.svg"
+          alt="ivy"
+          width={300}
+          height={500}
           priority
         />
+        <p>
+          za patero hlavami, sedmero lavičkami a devatero prázdnými kelímky byla
+          v zajetí nudy opilá princezna. Když v tom se náhle objevil <Link
+            style={{
+              textDecoration: "underline",
+            }}
+            href="https://youtube.com/clip/UgkxRlmhBEZqDuobYiH7_D7_UuF0g2zZMjaZ?si=DqVDt4DkLeEYPbBj"
+          >
+             Splašený William
+          </Link>{" "}
+          se svými{" "}
+          <Link
+            style={{
+              textDecoration: "underline",
+            }}
+            href="https://youtube.com/clip/UgkxJgSKGa1dxdC0_jsMJifRDglgp9Dmhrkq?si=2P253yw70zcsO6J7"
+          >
+            osmačtyřiceti Okouny.
+          </Link>
+          To přilákalo pozornost našeho hlavního hrdiny a jeho nohsleda. Po pár
+          obratných manévrech se princezně podařilo získat pozornost a zájem
+          našeho hrdiny, který měl zrovna náladu někomu zkomplikovat život.
+          Odvlekl jí do svého oblíbeného stánku (toho s tou dobrou hopsinkovou
+          šťávou) a pomocí několika správně vybraných lektvarů a dobře volených
+          slovních obratů jí ukázal, že on je tou správnou volbou. Dřív než si
+          stihla uvědomit, co se vlastně děje, vyměnili si čísla svých
+          poštovních holubů. Naštěstí pro našeho hrdinu, princezna má takový nešvar -
+          považuje za neslušné neodpovědět na zprávu - a tak to všechno začalo.
+        </p>
+        <br/>
+        Tahle pohádka není o nás, my jsme do sebe vrazili na Majálesu a dali se
+        do řeči... pak už to šlo samo.
+        <Image
+          className="relative "
+          src="/coincidence-i-think-not.gif"
+          alt="ivy"
+          width={500}
+          height={500}
+          priority
+        />
+        <br/>
+        <br/>
+        TLDR:
+        <div style={{width: "100%", height: "25vh", position: "relative"}}>
+        <Image
+          className="relative "
+          src="/tldr.svg"
+          alt="tldr"
+          fill
+        />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div id="plan" className="flex  flex-col items-center justify-between">
+        <h2>Plán</h2>
+        <Image
+          className="relative "
+          src="/divider.svg"
+          alt="ivy"
+          width={200}
+          height={500}
+          priority
+        />
+        <div className="mb-32 text-center container-section-1 ">
+          <div className="container-section-1-left">
+            <div className="section-1-left-line">Úterý 25.6.2024</div>
+            <br />
+            <br />
+            <div className="section-1-left-line">Středa 26.6.2024</div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="section-1-left-line">Čtvrtek 27.6.2024</div>
+          </div>
+          <div className="container-section-1-right">
+            <div className="section-1-right-line">16:00 Příjezd</div>
+            <div className="section-1-right-line">
+              18:00 Grilování a warmup na den D
+            </div>
+            <br />
+            <div className="section-1-right-line">8:00 Snídaně</div>
+            <div className="section-1-right-line">11:00 Obřad</div>
+            <div className="section-1-right-line">11:30 Focení</div>
+            <div className="section-1-right-line">12:30 Oběd</div>
+            <div className="section-1-right-line">15:00 Hod kyticí</div>
+            <div className="section-1-right-line">15:30 První tanec</div>
+            <div className="section-1-right-line">16:00 Volná zábava</div>
+            <div className="section-1-right-line">20:00 Večerní raut</div>
+            <div className="section-1-right-line">20:00 Focení novomanželů</div>
+            <div className="section-1-right-line">21:30 Prskavky</div>
+            <div className="section-1-right-line">21:45 Párty</div>
+            <br />
+            <div className="section-1-right-line">11:00 Opuštění pokojů</div>
+            <div className="section-1-right-line">14:00 Odjezd</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div id="map" className="flex  flex-col items-center justify-between">
+          <h2>Kdepa to bude</h2>
+          <iframe
+            width="100%"
+            height="450"
+            loading="lazy"
+            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ-8RddWzUCkcRrbrw-VkaitE&key=AIzaSyDcGyXB3E5WYzIzDbHAAzcBv96WucQPBNU"
+          ></iframe>
+        </div>
+      </div>
+      <div
+        id="colors"
+        className="flex  flex-col items-center justify-between mb-16"
+      >
+        <h2>Barvy</h2>
+        Svatební výzdoba bude laděna do těchto barev
+        <div className="text-center container-section-1 gap-8 ">
+          <div className="color-circle color-1"></div>
+          <div className="color-circle color-2"></div>
+          <div className="color-circle color-3"></div>
+          <div className="color-circle color-4"></div>
+          <div className="color-circle color-5"></div>
+        </div>
+        Nikoho nebude nutit, ALE nevěsta bude ráda, když se budete držet těchto
+        barev
+      </div>
+      <div
+        id="form"
+        className="flex  flex-col items-center justify-between  mb-16"
+      >
+        <h2>Formulář</h2>
+        Pokud se chystáš přijet na naší svatbu, vyplň prosím následující
+        formulář
+        <Link
+          style={{
+            textDecoration: "underline",
+            color: "blue",
+            fontSize: 30,
+          }}
+          href="https://forms.gle/s26dvhcrHsRYmfqe9"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          ZDE
+        </Link>
       </div>
     </main>
   );
